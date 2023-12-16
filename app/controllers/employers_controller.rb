b220cc2df02224b33263ea17772849fb972715a3
Michael Marchand <35391349+MarchandMD@users.noreply.github.com>
2023-12-15 22:29:58 -0700

    some chagnes

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -2,4 +2,25 @@ class EmployersController < ApplicationController
   def index
     @employers = Employer.sort_by_name
   end
+
+  def new
+  end
+
+  def create
+    @employer = Employer.new(employer_params)
+
+    if @employer.save
+      flash[:success] = "Congrats! #{@employer.name} has been added to the list"
+      redirect_to index
+    else
+      render index
+      flash[:alert] = "please try again"
+    end
+  end
+
+  private
+
+  def employer_params
+    params.permit(:name, :city, :state, :website, :industry, :employees)
+  end
 end
