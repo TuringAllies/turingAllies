55b09c41d150688a932361b386b408bf0401ac59
Michael Marchand <35391349+MarchandMD@users.noreply.github.com>
2023-12-23 10:39:05 -0700

    removes users from database

--- db/schema.rb
+++ db/schema.rb
@@ -26,18 +26,4 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_22_041755) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "provider"
-    t.string "uid"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
 end
