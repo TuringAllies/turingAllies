5bfa0f907ee8d8acb24c3d0649e199c205159fc9
Michael Marchand <35391349+MarchandMD@users.noreply.github.com>
2023-12-28 18:55:49 -0700

    enables omniauth with github

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
-  # devise :omniauthable, omniauth_providers: ["github"]
+  devise :omniauthable, omniauth_providers: %i[github]
 
   def self.from_omniauth(auth)
-    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails,
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+    if auth.info.email.present?
+      find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0, 20]
+        user.name = auth.info.name
+      end
+    else
+      find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+        user.email = "no_public_github@email.com"
+        user.password = Devise.friendly_token[0, 20]
+        user.name = auth.info.name
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root "welcome#index"
-  # devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
   get "about", to: "about#index", as: :about
 
   resources :employers, only: %i[index new create]
   resources :interviewers, only: %i[index]
+  resources :users
 end
