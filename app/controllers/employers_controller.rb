c44fdb0b201b2e2b7562ce72d36c3bb8fb5efbc5
Michael Marchand <35391349+MarchandMD@users.noreply.github.com>
2024-01-01 10:27:17 -0700

    applies flash messages during happy and sad path employer creation via turbo streams

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -9,13 +9,14 @@ class EmployersController < ApplicationController
   def create
     @employer = Employer.new(employer_params)
 
-    if @employer.save
-      @pagy, @employers = pagy(Employer.sort_by_name)
-      respond_to do |format|
+    @pagy, @employers = pagy(Employer.sort_by_name)
+
+    respond_to do |format|
+      if @employer.save
         format.turbo_stream { flash.now[:success] = "The employer #{@employer.name} was successfully added." }
+      else
+        format.turbo_stream { flash.now[:alert] = "Please try again" }
       end
-    else
-      render index, alert: "Please try again"
     end
   end
 
