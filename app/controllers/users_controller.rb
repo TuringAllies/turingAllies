6641cd644c8df83cc912eeb1398d87fec3377167
Michael Marchand <35391349+MarchandMD@users.noreply.github.com>
2025-03-09 10:32:11 -0600

    edits a users profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :first_name, :last_name, :cohort, :program, :bio)
+  end
 end
